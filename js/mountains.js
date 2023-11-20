9b2a1b04bf960c48858ed9474abdc133b3ed316c
RavynRouse <146771994+RavynRouse@users.noreply.github.com>
2023-11-20 00:20:18 -0600

    second

--- js/mountains.js
+++ js/mountains.js
@@ -1,22 +1,38 @@
-// data for mountains
-const mountainsData = [
-    { name: 'Mount Rainier', elevation: '14,411 feet', location: 'Washington' },
-    { name: 'Grand Teton', elevation: '13,775 feet', location: 'Wyoming' },
-    // Add more mountains as needed
+document.addEventListener('DOMContentLoaded', function () {
+  const mountainList = document.getElementById('mountain-list');
+
+  // Sample mountain data (you can replace this with a more extensive list)
+  const mountains = [
+      { name: 'Mount Everest', elevation: 29029, location: 'Himalayas' },
+      { name: 'Denali', elevation: 20310, location: 'Alaska' },
+      { name: 'Mount Rainier', elevation: 14410, location: 'Washington' },
+      // Add more mountains as needed
   ];
-  
-  // Function to display mountains in the list
-  function displayMountains(mountains) {
-    const mountainList = document.getElementById('mountainList');
-    mountainList.innerHTML = '';
-  
-    mountains.forEach(mountain => {
-      const listItem = document.createElement('li');
-      listItem.textContent = `${mountain.name} - Elevation: ${mountain.elevation}, Location: ${mountain.location}`;
-      mountainList.appendChild(listItem);
-    });
+
+  // Function to create mountain list items
+  function createMountainListItem(mountain) {
+      const listItem = document.createElement('div');
+      listItem.classList.add('mountain-item');
+
+      const nameElement = document.createElement('h2');
+      nameElement.textContent = mountain.name;
+
+      const elevationElement = document.createElement('p');
+      elevationElement.textContent = `Elevation: ${mountain.elevation} feet`;
+
+      const locationElement = document.createElement('p');
+      locationElement.textContent = `Location: ${mountain.location}`;
+
+      listItem.appendChild(nameElement);
+      listItem.appendChild(elevationElement);
+      listItem.appendChild(locationElement);
+
+      return listItem;
   }
-  
-  // Initial display of all mountains
-  displayMountains(mountainsData);
-  
+
+  // Populate mountain list
+  mountains.forEach(mountain => {
+      const mountainItem = createMountainListItem(mountain);
+      mountainList.appendChild(mountainItem);
+  });
+});
--- js/national_parks.js
+++ js/national_parks.js
@@ -1,37 +1,38 @@
-// data for national parks
-const nationalParks = [
-    { name: 'Yosemite National Park', location: 'California', type: 'National Park' },
-    { name: 'Zion National Park', location: 'Utah', type: 'National Park' },
-    // Add more national parks as needed
+document.addEventListener('DOMContentLoaded', function () {
+  const parkList = document.getElementById('park-list');
+
+  // Sample national park data (you can replace this with a more extensive list)
+  const nationalParks = [
+      { name: 'Yellowstone National Park', location: 'Wyoming', type: 'National Park' },
+      { name: 'Yosemite National Park', location: 'California', type: 'National Park' },
+      { name: 'Grand Canyon National Park', location: 'Arizona', type: 'National Park' },
+      // Add more national parks as needed
   ];
-  
-  // Function to display parks based on user's search
-  function searchParks() {
-    const locationFilter = document.getElementById('location').value;
-    const parkTypeFilter = document.getElementById('parkType').value;
-  
-    const filteredParks = nationalParks.filter(park => {
-      const locationMatch = locationFilter === 'all' || park.location === locationFilter;
-      const typeMatch = parkTypeFilter === 'all' || park.type === parkTypeFilter;
-  
-      return locationMatch && typeMatch;
-    });
-  
-    displayParks(filteredParks);
+
+  // Function to create park list items
+  function createParkListItem(park) {
+      const listItem = document.createElement('div');
+      listItem.classList.add('park-item');
+
+      const nameElement = document.createElement('h2');
+      nameElement.textContent = park.name;
+
+      const locationElement = document.createElement('p');
+      locationElement.textContent = `Location: ${park.location}`;
+
+      const typeElement = document.createElement('p');
+      typeElement.textContent = `Type: ${park.type}`;
+
+      listItem.appendChild(nameElement);
+      listItem.appendChild(locationElement);
+      listItem.appendChild(typeElement);
+
+      return listItem;
   }
-  
-  // Function to display parks in the list
-  function displayParks(parks) {
-    const parkList = document.getElementById('parkList');
-    parkList.innerHTML = '';
-  
-    parks.forEach(park => {
-      const listItem = document.createElement('li');
-      listItem.textContent = `${park.name} - ${park.location} (${park.type})`;
-      parkList.appendChild(listItem);
-    });
-  }
-  
-  // Initial display of all parks
-  displayParks(nationalParks);
-  
+
+  // Populate park list
+  nationalParks.forEach(park => {
+      const parkItem = createParkListItem(park);
+      parkList.appendChild(parkItem);
+  });
+});
